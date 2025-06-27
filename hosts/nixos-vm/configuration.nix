bf47f6741afb2b421ab9162242312e4438caf97b
Peter Edley <peter@edleyit.com>
2025-06-27 10:11:28 +0100

    change load order

--- hosts/nixos-vm/configuration.nix
+++ hosts/nixos-vm/configuration.nix
@@ -7,8 +7,8 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./disko-config.nix                       
+      ./disko-config.nix  
+      ./hardware-configuration.nix                    
     ];
 
   # Bootloader.
